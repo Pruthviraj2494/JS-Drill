f9593ad31a82db4f682e8de639fceb2b60d1c520
Pruthviraj2494 <pruthvirajnaiks1@gmail.com>
2022-08-19 13:47:24 +0530

    completed problem 6

---
+++ problem6.js
@@ -0,0 +1,6 @@
+const testProblem6 = require("./test/testProblem6.js");
+const inventory = require("./inventoryData")
+
+const result = testProblem6(inventory);
+
+console.log(JSON.stringify(result))
